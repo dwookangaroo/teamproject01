24d236db7d8328c45a1d8d1db59c061a45a28e6a
강동우 <dwk9181@yahoo.com>
2021-07-20 14:51:36 +0900

    test

--- teamproject/home/urls.py
+++ teamproject/home/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
-
+    path('board/', views.board_view, name='board_view'),
+    path('usa/', views.usa_view, name='usa_view')
 ]
--- teamproject/home/views.py
+++ teamproject/home/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'home.html')
 
+
+def board_view(request):
+    return render(request, 'index.html')
+
+def usa_view(request):
+    return render(request, 'usa.html')
