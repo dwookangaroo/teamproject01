acbaf54cae024dbdb70994ae86c1d38d7001f8aa
sage5632 <sage5632@gmail.com>
2021-07-20 12:42:18 +0900

    TeamProject

--- teamproject/teamproject/urls.py
+++ teamproject/teamproject/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', include('home.urls')),
+    path('home/', include('home.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
