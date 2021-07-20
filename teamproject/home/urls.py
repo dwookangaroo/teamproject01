72c7d9ceecd8bfbc239b84a3cc54e975efce937b
sage5632 <sage5632@gmail.com>
2021-07-20 17:51:21 +0900

    TeamProject

--- teamproject/home/urls.py
+++ teamproject/home/urls.py
@@ -5,5 +5,7 @@ app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
     path('board/', views.board_view, name='board_view'),
-    path('usa/', views.usa_view, name='usa_view')
+    path('usa/', views.usa_view, name='usa_view'),
+    path('recidivism/', views.recidivism_view, name='recidivism_view'),
+    path('write', views.write_article, name='write_article'),
 ]
--- teamproject/home/views.py
+++ teamproject/home/views.py
@@ -6,9 +6,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-
 def board_view(request):
     return render(request, 'index.html')
 
 def usa_view(request):
     return render(request, 'usa.html')
+
+def recidivism_view(request):
+    return render(request, 'recidivism.html')
+
+def write_article(request):
+    return render(request, 'writeArticle.html')
